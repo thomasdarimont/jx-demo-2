565d736cd9547eb2c70d6a210d9068558ce8d966
Thomas Darimont <thomas.darimont@codecentric.de>
2019-08-08 19:39:08 +0200

    Add Greeting controller

--- src/main/java/com/github/thomasdarimont/training/jx/jxdemo2/DemoApplication.java
+++ src/main/java/com/github/thomasdarimont/training/jx/jxdemo2/DemoApplication.java
@@ -2,12 +2,31 @@ package com.github.thomasdarimont.training.jx.jxdemo2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
+}
+
+@RestController
+class GreetingController {
+
+    @GetMapping("/")
+    Object greet(@RequestParam(defaultValue = "World") String name) {
+        Map<Object, Object> map = new HashMap<>();
+        map.put("greeting", "Hello " + name);
+        map.put("timestamp", Instant.now());
+        return map;
+    }
 }
